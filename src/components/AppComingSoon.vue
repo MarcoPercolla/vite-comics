10b3ce33e9678cbae5cf44d417c1c84c81abf898
ReDMallard <marco@titori.it>
2023-11-30 00:45:13 +0100

    aggiunge jumbo e css base

--- src/components/AppComingSoon.vue
+++ src/components/AppComingSoon.vue
@@ -1,14 +1,23 @@
 <script></script>
 <template>
     <section class="comingSoon">
-        <h2>Coming Soon...</h2>
+        <img class="jumbo" src="../../img/jumbotron.jpg" alt="">
     </section>
 </template>
 <style scoped>
 .comingSoon {
-    height: 6rem;
-    background-color: rgb(32, 32, 32);
-    color: white;
-    padding: 3rem 10%;
+
+    width: 100%;
+    display: block;
+
+
+}
+
+.jumbo {
+    height: 18rem;
+    width: 100%;
+    object-fit: cover;
+    object-position: top;
+    display: block;
 }
 </style>
