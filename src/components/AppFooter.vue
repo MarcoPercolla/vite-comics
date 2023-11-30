e8fde60da869a36102a11c7338a12237e5f4e6e2
ReDMallard <marco@titori.it>
2023-11-30 01:02:16 +0100

    aggiunge lista link footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -19,12 +19,51 @@ export default {
 </script>
 <template>
     <div class="murales">
-        <div class="footerLinks"></div>
-        <div class="bgLogo">
-
+        <div class="footerLinks">
+            <ul>
+                <h5>DC COMICS</h5>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+                <ul>
+                    <h5>SHOP</h5>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+            </ul>
+            <ul>
+                <h5>DC</h5>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy policy (New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent Workshops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+            <ul>
+                <h5>SITES</h5>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+            </ul>
         </div>
+
+        <div class="bgLogo"> </div>
     </div>
 </template>
+
+
 <style scoped>
 .murales {
     display: flex;
@@ -43,5 +82,31 @@ export default {
     background-size: cover;
     background-position: center;
     background-image: url("../../img/dc-logo-bg.png");
+}
+
+.footerLinks {
+
+    display: flex;
+    margin: 2rem;
+}
+
+h5 {
+    margin: 0.8rem 0;
+    color: white;
+}
+
+ul {
+    margin: 0 0.5rem;
+}
+
+li {
+    list-style: none;
+    margin: 0.2rem;
+
+}
+
+a {
+    color: grey;
+    font-size: 0.7rem;
 }
 </style>
