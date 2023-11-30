2d81c517e4652cde5e699daa5c9475c4045c9370
ReDMallard <marco@titori.it>
2023-11-30 02:30:22 +0100

    aggiunge "bottoni" in CardDisplay

--- src/components/AppCardDisplay.vue
+++ src/components/AppCardDisplay.vue
@@ -10,17 +10,58 @@ export default {
 }
 </script>
 <template>
-    <div class="cardsContainer">
-        <AppComicCard v-for="comic in catalog" :comicId="comic" />
+    <section class="dcCards">
+        <div class="fakeBtn">
+            <h2>CURRENT SERIES</h2>
+        </div>
+        <div class="cardsContainer">
+            <AppComicCard v-for="comic in catalog" :comicId="comic" />
 
-    </div>
+        </div>
+        <div class="fakeBtn2">
+            <h2>LOAD MORE</h2>
+        </div>
+    </section>
 </template>
 <style scoped>
 .cardsContainer {
-    background-color: #222;
+
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
-    padding: 4rem 10%;
+    padding: 0.5rem 10%;
+}
+
+.dcCards {
+    background-color: #222;
+    position: relative;
+    padding: 2rem 0;
+
+}
+
+.fakeBtn {
+    width: 12rem;
+    height: 2.5rem;
+    font-size: 0.8rem;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(0, 110, 255);
+    position: absolute;
+    top: -1rem;
+    left: 10%
+}
+
+.fakeBtn2 {
+    width: 10rem;
+    height: 2.5rem;
+    font-size: 0.6rem;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(0, 110, 255);
+    margin: 0.5rem auto;
 }
 </style>
